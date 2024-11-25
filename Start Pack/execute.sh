35abeec08dadd1212654f06319db7f80926ddce5
Smara Tasmoc <fo21366@bc4login3.bc4.acrc.priv>
2024-11-25 15:02:38 +0000

    add executable

---
+++ Start Pack/execute.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=lab1
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH -o ./log_%j.out # STDOUT out
+#SBATCH -e ./log_%j.err # STDERR out
+#SBATCH --gres=gpu:1
+#SBATCH --time=0:10:00
+#SBATCH --mem=2GB
+#SBATCH --account=COMS033444
+
+module load languages/python/tensorflow-2.16.1
+echo "Start"
+python download.py
+echo "Done"
